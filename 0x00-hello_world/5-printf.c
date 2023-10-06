63e6f36a5f82741035d31958cb03b03b206d5825
moonshot7 <dardouri.mery@gmail.com>
2023-10-05 18:29:01 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+/**
+ * main - this is a prinf function
+ * Return: 0
+ */
+
+int main(void)
+{
+	pritnf("with proper grammar, but the outcome is a piece of art,");
+		return (0);
+}
