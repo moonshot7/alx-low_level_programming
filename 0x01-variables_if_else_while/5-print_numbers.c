3ba555adc571760f4164831a8295fcf32362a2bb
moonshot7 <dardouri.mery@gmail.com>
2023-10-06 17:22:15 -0700

    numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -9,15 +9,12 @@
 
 int main(void)
 {
-	char letter;
+	int a;
 
-	for (letter = 'a'; letter <= 'z'; letter++)
-
+	for (a = 0; a < 10; a++)
 	{
-	if (letter != 'e' && letter != 'q')
-	putchar(letter);
+		putchar(a + '0');
 	}
-
 	putchar('\n');
 
 	return (0);
