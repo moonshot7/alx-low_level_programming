a2ec1424185949e09bf108eb9c90883228a86e22
moonshot7 <dardouri.mery@gmail.com>
2023-10-11 16:12:07 -0700

    task2

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,15 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - Entry point of the program
- *
- * This function is the entry point for the program. It prints a message
- * to the console and returns 0.
- *
- * (no parameters needed for this function)
- * Member '\\o/\n': Represents a visual emoticon of joy and celebration.
- * Return: 0 on success
- */
+* main - causes an infinite loop
+* Return: 0
+*/
 
 int main(void)
 {
@@ -17,13 +11,13 @@ int i;
 
 printf("Infinite loop incoming :(\n");
 /**
-* i = 0;
-
-* while (i < 10)
-* {
+*i = 0;
+*while (i < 10)
+*{
 * putchar(i);
-* }
+*}
 */
 printf("Infinite loop avoided! \\o/\n");
+
 return (0);
 }
